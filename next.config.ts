30aaee9f0e789fa0f1500923b5779b0ca05068d1
RIfkiND <122434422+RIfkiND@users.noreply.github.com>
2025-08-24 19:55:52 +0700

    delete some component

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,17 @@
 import type { NextConfig } from "next";
+import withBundleAnalyzer from "@next/bundle-analyzer";
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-    ],
+    remotePatterns: [{ protocol: "https", hostname: "**" }],
   },
+  experimental: {
+    workerThreads: false,
+    cpus: 1,
+  },
+  swcMinify: true,
 };
 
-export default nextConfig;
+export default withBundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+})(nextConfig);
